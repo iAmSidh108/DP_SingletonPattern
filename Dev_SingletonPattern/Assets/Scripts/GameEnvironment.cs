7a5606b63fee14faf52581bf0593c9f3a1681ac8
Siddharth Tiwari <sidh589@gmail.com>
2024-02-18 19:48:41 +0530

    Setup an static GameEnvironment class to hold all the singleton Objects.

---
+++ Dev_SingletonPattern/Assets/Scripts/GameEnvironment.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    //This scripts will contain all the information related to things that are going to be there in scene all the time.
+    private static GameEnvironment instance;
+
+    private List<GameObject> obstacles = new List<GameObject>();
+    public List<GameObject> Obstacles { get { return obstacles; } }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                
+            }
+            return instance;
+
+        }
+    }
+
+    public void AddObstacles(GameObject go)
+    {
+        obstacles.Add(go);
+    }
+
+    public void RemoveObstacles(GameObject go)
+    {
+        int index = obstacles.IndexOf(go);
+        obstacles.RemoveAt(index);
+        GameObject.Destroy(go);
+    }
+
+}
